db83028a8a32e3facda596868b7a685da554d625
Biswarup <biswarupmaity2004official@gmail.com>
2025-10-24 11:23:30 +0530

    home controller added

---
+++ src/main/java/com/bity/bitykart/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.bity.bitykart.controller;
+
+import com.bity.bitykart.dto.OrderDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String Home(){
+        return "Welcome to bity kart";
+    }
+}
